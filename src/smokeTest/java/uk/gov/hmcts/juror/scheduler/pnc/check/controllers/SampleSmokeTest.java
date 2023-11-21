37e651e5c4d0ae1a010a16dd5d404e646b2acbde
Neil Perry <neil.perry@cgi.com>
2023-11-21 08:44:26 +0000

    Revert "re-align with jcde" Fix smokeTest checkstyle issue. This reverts commit 8ab6e34bf17130a619a037770334089fe1ca67e4.

--- src/smokeTest/java/uk/gov/hmcts/juror/scheduler/pnc/check/controllers/SampleSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/juror/scheduler/pnc/check/controllers/SampleSmokeTest.java
@@ -4,7 +4,6 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
